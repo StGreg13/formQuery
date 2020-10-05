be54a3ec8cdf89942ef469b91695501b9e28da44
StGreg13 <13mrgreg@gmail.com>
2020-10-05 11:13:26 +0300

    bootstrap popup-form

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-var link = $('.modal-link'),
-    popup = $('.popup'),
-    overlay = $('.popup-overlay'),
-    close = $('.popup-close');
-
-
-link.click(function(){
-    overlay.fadeIn();
-    popup.toggleClass('popup-active');
-});
-close.click(function(){
-    overlay.fadeOut();
-    popup.toggleClass('popup-active');
-});
-overlay.click(function(){
-    overlay.fadeOut();
-    popup.toggleClass('popup-active');
-});
+$('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text('New message to ' + recipient)
+    modal.find('.modal-body input').val(recipient)
+  })
